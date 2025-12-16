481527610ddefcc745442a2740d03e9e52a878ab
David Courtey <david_courtey@yahoo.com>
2025-12-16 22:20:03 +0100

    chore: add Apple Touch Icon support for layouts and include new `apple-touch-icon.png`
    - Update `app.blade.php` and `auth.blade.php` to include Apple Touch Icon link.

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
     <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
-
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}/">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen bg-base-200 antialiased">
